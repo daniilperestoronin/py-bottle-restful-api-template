e2a1d29dbbe193ce6fd50ec557d63252396fb2a6
Daniil Perestoronin <perestoronin.daniil@gmail.com>
2017-11-19 15:38:30 +0300

    notes_test_data.sql added

--- notes.sql
+++ notes.sql
@@ -1,8 +1,9 @@
 CREATE DATABASE notes;
-CREATE USER notes_user WITH password 'order_user';
+CREATE USER notes_user WITH password 'notes_user';
 GRANT ALL ON DATABASE notes TO notes_user;
 
 CREATE TABLE notes(
 	id bigserial PRIMARY KEY,
 	creation_date DATE NOT NULL,
-  note_text TEXT NOT NULL);
+	note_header VARCHAR(255) NOT NULL,
+    note_text TEXT NOT NULL);
---
+++ notes_test_data.sql
@@ -0,0 +1,5 @@
+INSERT INTO notes (note_header, note_text)
+VALUES
+  ('test_header_1, test_text_1'),
+  ('test_header_2, test_text_2'),
+  ('test_header_3, test_text_3');
